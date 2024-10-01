5599bf5ec7a9a28cde90cae459668af2bd8454d2
Gabriel <gabriel.kitanovich@gmail.com>
2024-09-30 21:24:32 -0300

    Add Entities and new Packages

---
+++ src/main/java/com/example/demo/domain/entities/Domicilio.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Domicilio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String calle;
+    private int numero;
+    private String provincia;
+}
